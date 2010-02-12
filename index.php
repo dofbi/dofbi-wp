d82b8c44a6b2025d92976461df667f7a121b6f28
Mamadou Diagne <genova@dofbi.com>
2010-02-12 13:44:51 +0100

    Signed-off-by: Mamadou Diagne <genova@dofbi.com>

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
- * Dia
+ * 
  * @package WordPress
  */
 
